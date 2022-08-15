64169aa577ab2fb6d6655e81470f4b449012aafa
KennBro <kennbro@protonmail.com>
2022-08-15 13:23:30 -0300

    [FIX] Corrections

--- frontend/src/app/contract-code/contract-code.component.ts
+++ frontend/src/app/contract-code/contract-code.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { HighlightLoader, HighlightAutoResult } from 'ngx-highlightjs';
-// const themeHL: string = 'node_modules/highlight.js/styles/arduino-light.css';
-              // '../../../node_modules/highlight.js/styles/arduino-light.css']
 
 @Component({
   selector: 'app-contract-code',
@@ -16,24 +14,14 @@ export class ContractCodeComponent implements OnInit {
   codeRaw : any;
 
   response!: HighlightAutoResult;
-
   code!: string; 
-  //  = `function myFunction() {
-  //   document.getElementById("demo1").innerHTML = "Test 1!";
-  //   document.getElementById("demo2").innerHTML = "Test 2!";
-  // }`;
 
-  // currentTheme: string = themeHL;
-
-  constructor (private hljsLoader: HighlightLoader) { 
-  }
-
+  constructor (private hljsLoader: HighlightLoader) { }
 
   ngOnInit(): void {
     this.contract = this.data;
     console.log(`Contract : {this.contract}`, this.data);
     this.code = this.contract['SourceCode']
-    // console.log(`Theme : {this.currentTheme}`, this.currentTheme);
   }
 
 }
